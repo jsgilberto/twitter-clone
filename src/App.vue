0c6ed1121addf8b1ceeb11fb6c13eef64ff0c138
Jesus Alvarez <alv.mtz94@gmail.com>
2020-10-14 11:09:46 -0700

    added new vue component UserProfile

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="container">
+  <UserProfile/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserProfile from './components/UserProfile.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UserProfile
   }
 }
 </script>
@@ -21,8 +20,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <!-- <div class="user-profile">
+      <div class="user-profile__user-panel">
+        <h1 class="user-profile__username">@{{ user.username }}</h1>
+        <div class="user-profile__follower-count">
+          <strong>Followers:</strong> {{ followers }}
+        </div>
+      </div>
+    </div> -->
+
+    <h1>Profile</h1>
+    <b-card
+      :title="'@' + user.username"
+      tag="article"
+      class="mb-2"
+    >
+      <b-card-text>
+        {{ user.email }} <br>
+        <strong>Followers: </strong> {{ followers}}
+      </b-card-text>
+
+      <b-button href="#" variant="primary" @click="followUser" >Follow</b-button>
+    </b-card>
+  </div>
+  
+  
+</template>
+
+<script>
+export default {
+  name: 'UserProfile',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'chinitodaman',
+        firstName: 'Jesus',
+        lastName: 'Alvarez',
+        email: 'alv.mtz94@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    }
+  },
+  mounted() {
+    this.followUser();
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (newFollowerCount > oldFollowerCount)
+        console.log(`${this.user.username} has gained a follower!`);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.user-profile {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
